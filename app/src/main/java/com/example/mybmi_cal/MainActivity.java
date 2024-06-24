c16074d2b54669f08e760ad435dd74e4a398a7f9
Mohit kumar <145196903+Mohit-09-G@users.noreply.github.com>
2024-06-24 14:42:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mybmi_cal/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mybmi_cal;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText edtWeight , edtHeightFt , edtHeightIn;
+        Button btnCalculate;
+        TextView txtResult;
+
+        edtWeight =findViewById(R.id.edtWeight);
+        edtHeightFt =findViewById(R.id.edtHeightFt);
+        edtHeightIn = findViewById(R.id.edtHeightIn);
+        btnCalculate = findViewById(R.id.btnCalculate);
+
+         txtResult = findViewById(R.id.txtResult);
+
+
+
+         btnCalculate.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+
+                int wt = Integer.parseInt(edtWeight.getText().toString());
+                int ft =Integer.parseInt(edtHeightFt.getText().toString());
+                int in = Integer.parseInt(edtHeightIn.getText().toString());
+                int totalIn=ft*12+in;
+                double totalCm=totalIn*2.53;
+                double totalM=totalCm/100;
+                double bmi =wt/(totalM*totalM);
+                if (bmi>25){
+                    txtResult.setText("You are overweight");
+                } else if (bmi<18) {
+                    txtResult.setText("you are underweight ");
+                } else {
+                    txtResult.setText("you are healthy");
+                }
+             }
+         });
+
+    }
+}
